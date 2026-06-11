04add9f934ef61ea02e9a85736b3d76d2b06552b
Marcelo <mdbezerra16@gmail.com>
2026-06-11 15:22:43 -0300

    Muda nome do bucket s3

--- env/Homolog/Backend.tf
+++ env/Homolog/Backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-alura"
+    bucket = "terraform-state-alura-225498923960-us-west-2-an"
     key    = "Prod/terraform.tfstate"
     region = "us-west-2"
   }
